36c21263f22dff7a5a44b128b712ee7f70e852a8
june6723 <june6723@naver.com>
2021-04-28 10:52:43 +0900

    Join request

--- routes/communities.js
+++ routes/communities.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
-import { createNewCommunity, findCommunity, getCommunityPosts } from '../controllers/communityController.js';
+import { 
+  createNewCommunity,
+  findCommunity,
+  getCommunityPosts,
+  handleJoinRequest,
+  latestCommunities,
+  requestToJoinCommunity
+} from '../controllers/communityController.js';
 
 const router = express.Router();
 
 router.post('/', auth, createNewCommunity);
 router.get('/find', findCommunity);
 router.get('/:id/posts', getCommunityPosts);
+router.get('/latest', latestCommunities);
+router.patch('/:id/join', auth, requestToJoinCommunity);
+router.patch('/:id/join/:id', auth, handleJoinRequest);
 
 export default router;
