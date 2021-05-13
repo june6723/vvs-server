459705c32eb4d117d23946d8b9c897bb5ad57bdb
june6723 <june6723@naver.com>
2021-05-13 23:48:28 +0900

    comment route add

--- routes/Posts.route.js
+++ routes/Posts.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createPost, getMyPosts } from '../controllers/Post.controller.js';
+import { createPost, dislikePost, getMyPosts, likePost } from '../controllers/Post.controller.js';
 import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
 router.get('/myposts', auth, getMyPosts);
 router.post('/', auth, createPost);
+router.patch('/:id/likes', auth, likePost)
+router.patch('/:id/dislikes', auth, dislikePost)
 
 export default router;
 
