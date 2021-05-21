8e871531f13a0977754f6f8e67aad28b67415dfd
june6723 <june6723@naver.com>
2021-05-21 23:39:39 +0900

    Auth edit

--- controllers/Comment.controller.js
+++ controllers/Comment.controller.js
@@ -26,8 +26,7 @@ export const getComments = async (req, res, next) => {
   try {
     const { postId } = req.params
     const result = await Comment.find({ post: postId })
-      .populate('creator', { name:1, profileImg:1 }).populate('replies').exec()
-    console.log(result)
+      .populate('creator', { name:1, profileImg:1 }).exec()
     res.send(result)
   } catch (error) {
     next(error)
@@ -47,10 +46,10 @@ export const createReply = async (req, res, next) => {
       replyOn: commentId,
       replies: []
     })
-    const reply = await newComment.save()
-    const comment = await Comment.findByIdAndUpdate(commentId, { $push: { replies: reply._id }}, { new:true })
-      .populate('creator', 'replies').exec()
-    res.send(comment)
+    const { _id } = await newComment.save()
+    const reply = await Comment.findOne({ _id }).populate('creator').exec()
+    await Comment.findByIdAndUpdate(commentId, { $push: { replies: _id }})
+    res.send(reply)
   } catch (error) {
     next(error)
   }
@@ -65,12 +64,24 @@ export const likeComment = async (req, res, next) => {
     let updated
     if(comment.likes.includes(userId)) {
       updated = await Comment.findByIdAndUpdate(commentId, { $pull: { likes: userId }}, { new: true})
-        .populate('creator').populate('replies').exec()
+        .populate('creator').exec()
     } else {
       updated = await Comment.findByIdAndUpdate(commentId, { $push: { likes: userId }}, { new: true})
-        .populate('creator').populate('replies').exec()
+        .populate('creator').exec()
     }
     res.send(updated)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getReplies = async (req, res, next) => {
+  try {
+    const { commentId } = req.params
+
+    const replies = await Comment.find({ replyOn: commentId }).populate('creator').exec()
+    console.log(replies)
+    res.send(replies)
   } catch (error) {
     next(error)
   }
--- models/Comment.js
+++ models/Comment.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose; 
 
 const commentSchema = mongoose.Schema({
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   post: { type: Schema.Types.ObjectId, ref: 'Post'},
   text: { type: String, required: true, maxlength: 500 },
   likes: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
-  replyOn: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
+  replyOn: { type: Schema.Types.ObjectId, ref: 'Comment' },
   replies: [{ type: Schema.Types.ObjectId, ref: 'Comment', default: [] }]
 }, {timestamps: true});
 
