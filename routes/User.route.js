99797dbec89a17f58d3e0cc70dbd516c32445129
june6723 <june6723@naver.com>
2021-05-23 13:24:10 +0900

    Follow fn

--- routes/User.route.js
+++ routes/User.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { findUser, getJoinedCommunities } from '../controllers/User.controller.js';
+import { findUser, followOtherUser, getJoinedCommunities } from '../controllers/User.controller.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/:id/communities', auth, getJoinedCommunities);
 router.get('/find', findUser);
+router.patch('/follow/:followId', auth, followOtherUser)
 
 export default router;
